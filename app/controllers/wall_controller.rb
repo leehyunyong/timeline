bb648dc2fbaec981d5184416747d1b3a8abd923e
Hyunyong <flglrndl@gmail.com>
2014-03-04 21:52:48 +0900

    라우트 지정하기 및 오류 수정

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -23,7 +23,7 @@ class WallController < ApplicationController
     p.name = params[:writer_edit]
     p.content = params[:content_edit]
     if p.save
-      redirect_to "wall/posts"
+      redirect_to "/wall/posts"
     else
       flash[:alert] = p.errors[:content][0]
       redirect_to :back
